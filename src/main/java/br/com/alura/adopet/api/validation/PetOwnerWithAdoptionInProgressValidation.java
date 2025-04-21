3a78811673d659e904804c8bb789318cc41cf4bf
Thomas Palma <thomaspalma777@gmail.com>
2025-04-21 12:08:47 -0300

    feature(backend): add pet owner adoption in progress validation
    - created PetOwnerWithAdoptionInProgressValidation in validation package;
    - added validate method to check if pet owner has pending adoption;
    - implemented logic to throw ValidationException if pet owner has another adoption awaiting evaluation.

---
+++ src/main/java/br/com/alura/adopet/api/validation/PetOwnerWithAdoptionInProgressValidation.java
@@ -0,0 +1,36 @@
+package br.com.alura.adopet.api.validation;
+
+
+import br.com.alura.adopet.api.dto.AdoptionRequestDto;
+import br.com.alura.adopet.api.exception.ValidationException;
+import br.com.alura.adopet.api.model.Adoption;
+import br.com.alura.adopet.api.model.AdoptionStatus;
+import br.com.alura.adopet.api.model.PetOwner;
+import br.com.alura.adopet.api.repository.AdoptionRepository;
+import br.com.alura.adopet.api.repository.PetOwnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PetOwnerWithAdoptionInProgressValidation {
+
+    @Autowired
+    private AdoptionRepository adoptionRepository;
+
+    @Autowired
+    private PetOwnerRepository petOwnerRepository;
+
+    public void validate(AdoptionRequestDto dto) {
+        List<Adoption> adoptions = adoptionRepository.findAll();
+
+        PetOwner petOwner = petOwnerRepository.getReferenceById(dto.idPetOwner());
+
+        for (Adoption a : adoptions) {
+            if (a.getPetOwner() == petOwner && a.getStatus() == AdoptionStatus.PENDING_REVIEW) {
+                throw new ValidationException("Tutor already has another adoption awaiting evaluation!");
+            }
+        }
+    }
+}
