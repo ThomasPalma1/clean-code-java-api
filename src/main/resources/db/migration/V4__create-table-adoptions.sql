d31d38a9f7cea315cb732212a09d1882eeb3ade5
Thomas Palma <thomaspalma777@gmail.com>
2025-05-04 15:32:45 -0300

    [fix] remove default value from status column in adoptions table
    - removed the default value from the `status` column in the `adoptions` table.

--- src/main/resources/db/migration/V4__create-table-adoptions.sql
+++ src/main/resources/db/migration/V4__create-table-adoptions.sql
@@ -4,7 +4,7 @@ CREATE TABLE adoptions (
     pet_owner_id BIGINT NOT NULL,
     pet_id BIGINT NOT NULL,
     reason VARCHAR(255),
-    status VARCHAR(100) NOT NULL DEFAULT,
+    status VARCHAR(100) NOT NULL,
     justification_status VARCHAR(255),
     CONSTRAINT fk_adoptions_pet_owner_id FOREIGN KEY (pet_owner_id) REFERENCES pet_owners(id),
     CONSTRAINT fk_adoptions_pet_id FOREIGN KEY (pet_id) REFERENCES pets(id)
